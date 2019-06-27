9599d457b684f8f89db683dd82b0e9280fc81612
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-06-27 10:22:01 -0700

    Update stripe.js

--- src/Backend/Constants/stripe.js
+++ src/Backend/Constants/stripe.js
@@ -2,7 +2,7 @@ const configureStripe = require('stripe');
 
 const STRIPE_SECRET_KEY = process.env.NODE_ENV === 'production'
     ? 'sk_live_MY_SECRET_KEY'
-    :'src.env.STRIPE_SECRET_KEY';
+    :'sk_test_WF0I9UDegmxpsvlaputi1flr00riIpMjQw';
 
 const stripe = configureStripe(STRIPE_SECRET_KEY);
 
