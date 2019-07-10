d3e06d9f1d04f7377a8f7d267ce068298a492628
sshuman95 <49686103+sshuman95@users.noreply.github.com>
2019-07-10 11:50:01 -0700

    Update stripe.js

--- src/stripe.js
+++ src/stripe.js
@@ -1,5 +1,5 @@
 const STRIPE_PUBLISHABLE = process.env.NODE_ENV === 'production'
-  ? 'pk_test_kfycPlsK2S5nbrLMcFdoxriD006UR2lB7g'
-  : 'pk_test_kfycPlsK2S5nbrLMcFdoxriD006UR2lB7g';
+  ? '...'
+  : '...';
 
 export default STRIPE_PUBLISHABLE;
